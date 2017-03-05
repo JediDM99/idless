e0bb88c794f3cd4639bedf8c0437971c08806546
Matt Chodaczek <mchodaczek@gmail.com>
2017-03-05 14:16:43 -0500

    python timing works!

--- sms.py
+++ sms.py
@@ -3,6 +3,7 @@ from flask import render_template
 from flask import request
 from twilio.rest import TwilioRestClient
 import json
+import time
 
 app = Flask("sms sender")
  
@@ -11,7 +12,29 @@ ACCOUNT_SID = "ACe980192c091ed4100d6b581a169a55bf"
 AUTH_TOKEN = "59d21c92d86271a6d6ec8bb51c8f8040" 
  
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
- 
+
+def timer(timerName, seconds):
+	if(seconds>300):
+		time.sleep(seconds-300)
+		client.messages.create(
+	    	to="+16109370549", 
+	    	from_="+14848044148", 
+	    	body="Your " + timerName + " will be done in 5 minutes!", 
+		)
+		time.sleep(300)
+		client.messages.create(
+	    	to="+16109370549", 
+	    	from_="+14848044148", 
+	    	body="Your " + timerName + " is finished!", 
+		)
+	else:
+		time.sleep(seconds)
+		client.messages.create(
+	    	to="+16109370549", 
+	    	from_="+14848044148", 
+	    	body="Your " + timerName + " is finished!", 
+		)
+
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -29,6 +52,7 @@ def prelim():
 	    from_="+14848044148", 
 	    body="Your " + label + " will be done in " + hours + " hours and " + minutes + " minutes!", 
 	)
+    timer(label, int(hours)*3600 + int(minutes)*60)
     return json.dumps({"status": "OK"})
 
 #send the reminder message
@@ -37,11 +61,7 @@ def reminder():
     # Extract data from request
     label = request.json["label"]
     print "Received reminder for " + str(label)
-    client.messages.create(
-	    to="+16109370549", 
-	    from_="+14848044148", 
-	    body="Your " + label + " will be done in 5 minutes!", 
-	)
+
     return json.dumps({"status": "OK"})
 
 @app.route("/labels", methods=["POST"])
@@ -50,11 +70,7 @@ def labels():
     print request.json
     label = request.json["label"]
     print "Received label " + str(label)
-    client.messages.create(
-	    to="+16109370549", 
-	    from_="+14848044148", 
-	    body="Your " + label + " is finished!", 
-	)
+
     return json.dumps({"status": "OK"})
 
 
