8673cd871b03d7c282680b178ff2865dd10e0399
Matt Chodaczek <mchodaczek@gmail.com>
2017-03-05 09:23:55 -0500

    we can now send text messages!

--- sms.py
+++ sms.py
@@ -1,3 +1,6 @@
+from flask import Flask
+from flask import render_template
+from flask import request
 from twilio.rest import TwilioRestClient 
  
 # put your own credentials here 
@@ -7,8 +10,10 @@ AUTH_TOKEN = "59d21c92d86271a6d6ec8bb51c8f8040"
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
  
 client.messages.create(
-    to="+14843186843", 
-    from_="+16109370549", 
+    to="+16109370549", 
+    from_="+14848044148", 
     body="Test message", 
-    media_url="https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg", 
 )
+
+if __name__ == "__main__":
+    sms.run(debug=True)
